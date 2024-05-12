943f462d605bd14774b094d78699fd4c35aafe21
Jeremi <jeremiszczotka7@gmail.com>
2024-05-12 21:03:39 +0200

    Window of a game

---
+++ PONG/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "raylib.h"
+int main() {
+	const int screen_width = 1280;
+	const int screen_height = 800;
+	InitWindow(screen_width, screen_height, "PONG game");
+	SetTargetFPS(60);
+	while (WindowShouldClose() == false) {
+		BeginDrawing();
+		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+		DrawCircle(screen_width / 2, screen_height / 2, 20, WHITE);
+		DrawRectangle(10, screen_height / 2 - 60, 25, 120, WHITE);
+		DrawRectangle(screen_width-35, screen_height / 2 - 60, 25, 120, WHITE);
+
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
